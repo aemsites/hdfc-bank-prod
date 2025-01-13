1cab9f63ac927c9e67e9d44a5126810fc164221a
srivastava21ashish <srivastava21ashish@gmail.com>
2025-01-13 13:29:40 +0530

    Update constant.js

--- blocks/form/constant.js
+++ blocks/form/constant.js
@@ -15,7 +15,7 @@ export const defaultErrorMessages = {
   required: 'Please fill in this field.',
 };
 
-let submitBaseUrl = 'https://applyonlinestage.hdfcbank.com';
+let submitBaseUrl = 'https://applyonline.hdfcbank.com';
 
 const localDev = ['aem.live', 'aem.page', 'localhost', 'hlx.live', 'hlx.page'];
 const mainProd = ['hdfc-bank-prod'];
